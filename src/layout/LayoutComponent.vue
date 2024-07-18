b66e4841c71a5907fb02d659817b7218ff66f877
huynh.huu.hieu <>
2024-07-18 17:02:44 +0700

    second commit

--- src/layout/LayoutComponent.vue
+++ src/layout/LayoutComponent.vue
@@ -1,3 +1,5 @@
+<!-- @format -->
+
 <template>
   <div>
     <HeaderComponent
@@ -17,6 +19,30 @@
         <FooterComponent />
       </div>
     </main>
+    <div class="mobile-controls">
+      <div class="mobile-controls-main">
+        <div class="mobile-controls-item">
+          <home-icon />
+          <span>Home</span>
+        </div>
+        <div class="mobile-controls-item">
+          <home-icon />
+          <span>Home</span>
+        </div>
+        <div class="mobile-controls-center">
+          <div class="mobile-controls-center-round"><home-icon /></div>
+          <span>Home</span>
+        </div>
+        <div class="mobile-controls-item">
+          <home-icon />
+          <span>Home</span>
+        </div>
+        <div class="mobile-controls-item">
+          <home-icon />
+          <span>Home</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,6 +50,8 @@
 import HeaderComponent from "@/components/HeaderComponent.vue";
 import FooterComponent from "@/components/FooterComponent.vue";
 import SideBar from "@/components/SideBar.vue";
+import HomeIcon from "@/icons/HomeIcon.vue";
+
 export default {
   // Options...
   name: "LayoutComponent",
@@ -31,6 +59,7 @@ export default {
     HeaderComponent,
     FooterComponent,
     SideBar,
+    HomeIcon,
   },
   data() {
     return {
@@ -78,5 +107,53 @@ ul {
 .main-container {
   flex-grow: 1;
   width: 100%;
+}
+
+.mobile-controls {
+  height: 64px;
+  background-color: rgba(0, 0, 0, 1);
+  position: sticky;
+  bottom: 0;
+}
+
+.mobile-controls-main {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.mobile-controls-center {
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 19%;
+  position: relative;
+  top: -30%;
+}
+
+.mobile-controls-center-round {
+  height: 43px;
+  width: 43px;
+  background: rgba(128, 128, 128, 1);
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.mobile-controls-item {
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: Inter;
+  font-size: 10px;
+  font-weight: 500;
+  line-height: 12.1px;
+  text-align: left;
+  width: 19%;
 }
 </style>
